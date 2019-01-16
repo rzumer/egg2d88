973a7e7c47cdfe562f8599666677a83c80de48cf
Raphaël Zumer <rzumer@tebako.net>
2019-01-16 15:32:45 -0500

    Add Batch script automating conversion

---
+++ egg2d88.bat
@@ -0,0 +1,17 @@
+@ECHO off
+
+if "%1"=="" (
+    ECHO An input file is required.
+    EXIT -1
+)
+
+quickbms.exe -F "*.exe" -o project_egg.bms %~1 img
+
+set /A idx=0
+
+:loop
+    if not errorlevel 1 (
+        python egg2d88.py img/EGGFDIMG%idx% %~n1-%idx%.d88
+        set /A idx=idx+1
+        goto loop
+    )
